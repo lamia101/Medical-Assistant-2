d4a06a03b18f766fe07bb06e3373309aeba7d392
lamia101 <lamialatif01@gmail.com>
2024-10-22 13:05:04 +0600

    added first

--- app/src/main/java/com/example/medicalassistant/Home2.kt
+++ app/src/main/java/com/example/medicalassistant/Home2.kt
@@ -1,8 +1,10 @@
 package com.example.medicalassistant
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +17,27 @@ class Home2 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val home_report: CardView =findViewById(R.id.home_report)
+        val home_updateprofile:CardView=findViewById(R.id.home_updateprofile)
+        val home_medicalassistant:CardView=findViewById(R.id.home_medicalassistant)
+        val home_viewprofile:CardView=findViewById(R.id.home_viewprofile)
+
+        home_viewprofile.setOnClickListener {
+            val i=Intent(this,ViewProfile::class.java)
+            startActivity(i)
+        }
+        home_updateprofile.setOnClickListener {
+            val i=Intent(this,UpdateProfile::class.java)
+            startActivity(i)
+        }
+        home_report.setOnClickListener {
+            val i=Intent(this,Report::class.java)
+            startActivity(i)
+        }
+        home_medicalassistant.setOnClickListener {
+            val i=Intent(this,MedicalAssistant::class.java)
+            startActivity(i)
         }
     }
 }
